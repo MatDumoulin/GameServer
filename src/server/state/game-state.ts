98b2a92860723e64f8ff5295e0277bb3926010b2
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-08-21 15:14:15 -0400

    Beginning to work on the server library. For the moment, it is still coupled with a nodejs server which is not desired. Next step is to separate it in its own library and add tests.

---
+++ src/server/state/game-state.ts
@@ -0,0 +1,29 @@
+import { createStore, Store, Reducer, Unsubscribe } from 'redux';
+import { GameAction } from './actions';
+
+/**
+ * This class holds the game state for a match. 
+ * T: An object that represents your game state.
+ */
+export class GameState<T> {
+    private _store: Store<T, GameAction>;
+
+    constructor(parentReducer: Reducer) {
+        if(!parentReducer) {
+            console.error("A reducer must be given to the game state in order")
+        }
+        this._store = createStore(parentReducer);
+    }
+    
+    getState(): T {
+        return this._store.getState();
+    }
+    
+    dispatch(action: GameAction): void {
+        this._store.dispatch(action);
+    }
+
+    subscribe(callback: () => void): Unsubscribe {
+        return this._store.subscribe(callback);
+    }
+}
